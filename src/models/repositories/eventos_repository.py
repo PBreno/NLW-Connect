99b831535c015084174e9d43604764eb5d668b71
Breno Francisco Rafael Pombo <breno.pombo@biofhitus.local>
2025-02-19 17:18:09 -0300

    feat: Implementation of database connection

---
+++ src/models/repositories/eventos_repository.py
@@ -0,0 +1,7 @@
+from src.models.configs.connection import DBConnectionHandler
+from src.models.entities.eventos import Eventos
+
+class EventosRepository:
+    def insert(self, event_name: str):
+        pass
+
